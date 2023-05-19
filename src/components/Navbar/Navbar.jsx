337edc90775e6d49ca00c07acb37d9f5b282542c
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-19 18:22:30 +0600

    Private Route setup with auth

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
         <div className="navbar bg-primary text-primary-content">
             <Link className="btn btn-ghost normal-case text-xl" to='/'>Auth Master</Link>
             <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+            <Link className="btn btn-ghost normal-case text-xl" to='/orders'>Orders</Link>
             <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
             <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
 
--- src/main.jsx
+++ src/main.jsx
@@ -12,18 +12,24 @@ import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
 import AuthProviders from './components/AuthProviders/AuthProviders.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children:[
+    children: [
       {
         path: '/',
         element: <Home></Home>
       },
       {
-        path: '/login', 
+        path: '/orders',
+        element: <PrivateRoute><Orders></Orders></PrivateRoute>
+      },
+      {
+        path: '/login',
         element: <Login></Login>
       },
       {
@@ -37,7 +43,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProviders>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </AuthProviders>
   </React.StrictMode>,
 )
